b6fcac2bfb7f09ce96d05f3e8464064cc4548b91
madkarmaa <madkarma99@gmail.com>
2026-01-16 12:12:50 +0100

    fix: don't warn for function return types and eol chars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,17 @@ import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier';
 export default defineConfig(
     { ignores: ['**/node_modules', '**/dist', '**/out'] },
     tseslint.configs.recommended,
-    eslintConfigPrettier
+    eslintConfigPrettier,
+    {
+        rules: {
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            'linebreak-style': 'off',
+            'prettier/prettier': [
+                'error',
+                {
+                    endOfLine: 'auto'
+                }
+            ]
+        }
+    }
 );
