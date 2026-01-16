2e8ebafdba4c8385726690a4e6db179a083f34b3
madkarmaa <madkarma99@gmail.com>
2026-01-16 11:28:12 +0100

    chore: format files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from '@electron-toolkit/eslint-config-ts'
 import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
 
 export default defineConfig(
-  { ignores: ['**/node_modules', '**/dist', '**/out'] },
-  tseslint.configs.recommended,
-  eslintConfigPrettier
+    { ignores: ['**/node_modules', '**/dist', '**/out'] },
+    tseslint.configs.recommended,
+    eslintConfigPrettier
 )
