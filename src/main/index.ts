54da63da559ac39e9d2402a272199b9f5bef4c25
madkarmaa <madkarma99@gmail.com>
2026-01-16 12:12:19 +0100

    fix: spoof user agent to Chrome 144

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,8 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron';
 import { join } from 'path';
-import { electronApp, optimizer, is } from '@electron-toolkit/utils';
+import { electronApp, optimizer } from '@electron-toolkit/utils';
 import icon from '../../resources/icon.png?asset';
+import { chromeUserAgent } from './utils';
 
 function createWindow(): void {
     // Create the browser window.
@@ -28,11 +29,13 @@ function createWindow(): void {
 
     // HMR for renderer base on electron-vite cli.
     // Load the remote URL for development or the local html file for production.
-    if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-        mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
-    } else {
-        mainWindow.loadFile(join(import.meta.dirname, '../renderer/index.html'));
-    }
+    // if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+    //     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
+    // } else {
+    //     mainWindow.loadFile(join(import.meta.dirname, '../renderer/index.html'));
+    // }
+
+    mainWindow.loadURL('https://web.whatsapp.com');
 }
 
 // This method will be called when Electron has finished
@@ -41,6 +44,10 @@ function createWindow(): void {
 app.whenReady().then(() => {
     // Set app user model id for windows
     electronApp.setAppUserModelId('com.electron');
+
+    // spoof Chrome user agent to avoid being detected as Electron
+    // https://stackoverflow.com/a/79406250
+    app.userAgentFallback = chromeUserAgent('144.0.0.0');
 
     // Default open or close DevTools by F12 in development
     // and ignore CommandOrControl + R in production.
---
+++ src/main/utils/index.ts
@@ -0,0 +1,2 @@
+export const chromeUserAgent = (version: `${number}.${number}.${number}.${number}`) =>
+    `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${version} Safari/537.36`;
