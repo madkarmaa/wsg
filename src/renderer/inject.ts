2df710bf18223812738448f057b5a1eb46e84624
madkarmaa <madkarma99@gmail.com>
2026-01-19 17:58:04 +0100

    refactor: simplify code

--- src/renderer/inject.ts
+++ src/renderer/inject.ts
@@ -3,44 +3,20 @@ import type { Mod, ModulesMap } from './types';
 
 const logger = taggedLogger('inject');
 
-async function bootstrap() {
+const bootstrap = async () => {
     logger.info('Waiting for WhatsApp Web internal modules...');
 
-    const waitForRequire = (): Promise<typeof window.require> =>
-        new Promise((resolve) => {
-            const check = () => {
-                if (typeof window.require === 'function') resolve(window.require);
-                else setTimeout(check, 100);
-            };
-            check();
-        });
+    const getModules = () => window.require?.('__debug')?.modulesMap as ModulesMap;
 
-    try {
-        const require = await waitForRequire();
-
-        const getModules = () => {
-            try {
-                const debugModule = require('__debug');
-                if (debugModule.modulesMap) return debugModule.modulesMap as ModulesMap;
-                return null;
-            } catch {
-                return null;
-            }
-        };
-
-        let modulesMap = getModules();
-        while (!modulesMap) {
-            await new Promise((resolve) => setTimeout(resolve, 100));
-            modulesMap = getModules();
-        }
-
-        logger.info('Internal modules intercepted');
-        return modulesMap;
-    } catch (err) {
-        logger.error('Failed to bootstrap:', err);
-        return null;
+    let modulesMap = getModules();
+    while (!modulesMap) {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+        modulesMap = getModules();
     }
-}
+
+    logger.info('Internal modules intercepted', modulesMap);
+    return modulesMap;
+};
 
 const loadMods = async (modules: ModulesMap | null) => {
     if (!modules) return;
