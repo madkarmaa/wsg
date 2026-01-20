6a4445da0417d3e87e918dcd417af45a852f3626
madkarmaa <madkarma99@gmail.com>
2026-01-20 16:04:18 +0100

    refactor: remove debug log

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,6 +1,5 @@
 import { APP_NAME } from './constants';
 
-const ogDebug = console.debug;
 const ogLog = console.log;
 const ogInfo = console.info;
 const ogWarn = console.warn;
@@ -10,7 +9,6 @@ export const taggedLogger = (tag: string) => {
     const prefix = `[${APP_NAME}/${tag.trim().toLowerCase()}]`;
 
     return {
-        debug: (...args: unknown[]) => ogDebug(prefix, ...args),
         log: (...args: unknown[]) => ogLog(prefix, ...args),
         info: (...args: unknown[]) => ogInfo(prefix, ...args),
         warn: (...args: unknown[]) => ogWarn(prefix, ...args),
