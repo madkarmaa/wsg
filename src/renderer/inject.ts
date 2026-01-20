c82a52f029bcb4468ba1b309f29d04ee67065ec7
madkarmaa <madkarma99@gmail.com>
2026-01-20 09:56:06 +0100

    refactor: add logging

--- src/renderer/inject.ts
+++ src/renderer/inject.ts
@@ -5,20 +5,25 @@ import type { Mod, ModulesMap } from './types';
 const logger = taggedLogger('inject');
 
 let req: typeof window.require | undefined;
-
 Object.defineProperty(window, 'require', {
     configurable: true,
     enumerable: true,
     get: () => req,
     set(value) {
+        logger.info('Detected change to window.require');
         req = value;
 
         queueMicrotask(() => {
             try {
+                logger.info(`Attempting to load ${WHATSAPP_DEBUG_MODULE} module...`);
+
                 const modules = req?.(WHATSAPP_DEBUG_MODULE)?.modulesMap as ModulesMap;
-                modules && loadMods(modules);
+                if (!modules) return;
+
+                logger.info(`${WHATSAPP_DEBUG_MODULE} module loaded, loading mods...`);
+                loadMods(modules);
             } catch (err) {
-                logger.error('Failed to load __debug module', err);
+                logger.error(`Failed to load ${WHATSAPP_DEBUG_MODULE} module`, err);
             }
         });
     }
