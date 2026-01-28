a1790b4d310bfdc10e26e2a919c19577eb329c46
madkarmaa <madkarma99@gmail.com>
2026-01-28 17:49:39 +0100

    refactor: simplify logic

--- src/lib/mods/dependencies/ab.ts
+++ src/lib/mods/dependencies/ab.ts
@@ -3,19 +3,15 @@ import { type ModDependency } from '@lib/mods';
 
 export const ABFlags = (() => {
     const flagOverwrites = new Map<string, unknown>();
-    const overwriteABFlag = (id: string, value: unknown) => {
-        id = id.trim();
-        flagOverwrites.set(id, value);
-    };
+    const overwriteABFlag = (id: string, value: unknown) => flagOverwrites.set(id.trim(), value);
 
     patchModule<{
         getABPropConfigValue: (id: string) => unknown;
     }>('WAWebABProps', (exports) => {
         const og = exports.getABPropConfigValue;
         exports.getABPropConfigValue = (id: string) => {
-            const result = og(id);
             if (flagOverwrites.has(id)) return flagOverwrites.get(id);
-            return result;
+            return og(id);
         };
     });
 
